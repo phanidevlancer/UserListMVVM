78264126b52e08573a8c8204edfcf5ab686a5622
Phanindra Durga <phanindra.d@hdworks.in>
2026-04-10 07:35:36 +0530

    fixed again and again

--- feature/user-list/src/main/java/com/phani/feature/userlist/presentation/UserListScreen.kt
+++ feature/user-list/src/main/java/com/phani/feature/userlist/presentation/UserListScreen.kt
@@ -40,7 +40,7 @@ fun UserListScreen(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("User List") }
+                title = { Text("User Listz") }
             )
         }
     ) { paddingValues ->
