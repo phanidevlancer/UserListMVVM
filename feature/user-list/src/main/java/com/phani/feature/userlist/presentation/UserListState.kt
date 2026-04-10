e78301ac9e4d65892ad95fc0c2d6c14e026041a1
Phanindra Durga <phanindra.d@hdworks.in>
2026-04-10 07:45:52 +0530

    hey

--- feature/user-list/src/main/java/com/phani/feature/userlist/presentation/UserListState.kt
+++ feature/user-list/src/main/java/com/phani/feature/userlist/presentation/UserListState.kt
@@ -5,5 +5,5 @@ import com.phani.feature.userlist.domain.model.User
 data class UserListState(
     val users: List<User> = emptyList(),
     val isLoading: Boolean = false,
-    val error: String? = null
+    val error: String?
 )
